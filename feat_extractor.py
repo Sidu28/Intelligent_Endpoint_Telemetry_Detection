a0f7f2fc5d1ed7211c9711bd57028bb6b7388847
Siddhartha Jagannath <kachapah@stanford.edu>
2021-03-14 22:24:14 -0400

    deleted visualization.py

--- feat_extractor.py
+++ feat_extractor.py
@@ -3,6 +3,9 @@ import sys
 import argparse
 import pandas as pd
 import uuid
+import uuid
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # PE file related imports
 import pefile
@@ -45,7 +48,25 @@ def main():
   directory = os.path.join(os.getcwd(), 'data')
   if not os.path.isdir(directory):
     os.mkdir(directory)
+
+
   df.to_csv(directory+'/features_'+str(uuid.uuid4())+".csv")
+  '''
+  for col_idx in range(0,10):
+      col = df.iloc[:,col_idx]
+      plt.hist(col, color='blue', edgecolor='black',
+               bins=int(180 / 5))
+      plt.savefig(str(uuid.uuid4())+'.png')
+
+  '''
+  directory = os.path.join(os.getcwd(), 'data/images')
+  if not os.path.isdir(directory):
+    os.mkdir(directory)
+
+  fig, axes = plt.subplots(ncols=10, figsize=(22.9, 5))
+  for ax, col in zip(axes, df.columns):
+    plot = sns.distplot(df[col], ax=ax)
+  plt.savefig('data/images/' + str(uuid.uuid4()) + ".png")
 
 if __name__ == '__main__':
   main()

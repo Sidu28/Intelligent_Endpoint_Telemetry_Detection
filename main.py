198ba429914180ed00b554fb6761cca6dc87a993
Siddhartha Jagannath <kachapah@stanford.edu>
2021-03-15 19:29:07 -0400

    combined main.py and feat-extract.py

--- main.py
+++ main.py
@@ -16,19 +16,64 @@ from features.virustotal import VirusTotalExtractor
 feature_extractors = {
   ASMExtractor: None,
   SectionInfoExtractor: None,
-  VirusTotalExtractor: None # should the API key be a keyword argument?
+  #VirusTotalExtractor: None # should the API key be a keyword argument?
 }
 
 if __name__ == '__main__':
+
   parser = argparse.ArgumentParser(description="Execute feature extraction for an input PE file")
-  parser.add_argument('file', type=str, help="Input PE file to extract features for")
+  parser.add_argument('--file', type=str, required=False, help="Input PE file to extract features for")
+  parser.add_argument('--dir', type=str, required=False, help="Directory containing PE files to extract features for")
+  parser.add_argument('--label', type=int, required=False, default=1, help="Label for the PE Files you are processing")
   args = parser.parse_args()
 
-  features = {}
+  if not args.file and args.dir:
 
-  for extractor in feature_extractors:
-    kwargs = feature_extractors[extractor]
-    e = extractor(args.file)
-    features.update(e.extract(kwargs=kwargs))
+    rows = []
 
-  pprint(features)
+    for file in os.listdir(args.dir):
+      if not file.startswith('.'):
+        file = os.path.join(args.dir, file)
+        features = {}
+        for extractor in feature_extractors:
+          kwargs = feature_extractors[extractor]
+          e = extractor(file)
+          features.update(e.extract(kwargs=kwargs))
+
+        rows.append(features)
+
+    # Create dataframe using the feature extractors
+    df = pd.DataFrame(rows)
+    df['label'] = args.label
+
+    directory = os.path.join(os.getcwd(), 'data')
+    if not os.path.isdir(directory):
+      os.mkdir(directory)
+
+    df.to_csv(directory + '/features_' + str(uuid.uuid4()) + ".csv")
+    directory = os.path.join(os.getcwd(), 'data/images')
+    if not os.path.isdir(directory):
+      os.mkdir(directory)
+
+    # Plot the distributions of the important features
+    fig, axes = plt.subplots(ncols=10, figsize=(22.9, 5))
+    for ax, col in zip(axes, df.columns):
+      plot = sns.distplot(df[col], ax=ax)
+    plt.savefig('data/images/' + str(uuid.uuid4()) + ".png")
+
+  elif args.file:
+      features = {}
+
+      for extractor in feature_extractors:
+        kwargs = feature_extractors[extractor]
+        e = extractor(args.file)
+        features.update(e.extract(kwargs=kwargs))
+
+      pprint(features)
+
+  else:
+    parser.error('check your command line arguments')
+
+
+
+
